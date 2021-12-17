ec8c1dff61a324f8e72ba19bb24e4e2a2edc881d
roxana <rcanizares08@gmail.com>
2021-12-17 16:22:21 +0100

    excercises about loop

--- 06_advanced_set_operations/excercise.py
+++ 06_advanced_set_operations/excercise.py
@@ -4,6 +4,7 @@ my_list = [25, 50, 25]
 # Create a tuple, called my_tuple, with a single value in it
 my_tuple = (10,)
 
+
 # Modify set2 so that set1.intersection(set2) returns {5, 77, 9, 12}
 set1 = {14, 5, 9, 31, 12, 77, 67, 8}
 set2 = {5, 9, 12, 77}
