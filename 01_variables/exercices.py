8f0b96e4accf6469ff3fbbe402cbf91179b814ea
roxana <rcanizares08@gmail.com>
2021-12-17 11:09:38 +0100

    ignore add

---
+++ 01_variables/exercices.py
@@ -0,0 +1,9 @@
+# Create two variables, var1 and var2, both with the same value.
+var1 = 13
+var2 = 13
+
+# Create two variables, num1 and num2, which multiply together to give 16.
+num1 = 8
+num2 = 2
+result = num1 * num2
+print(result)
--- 04_first_python_app/code.py
+++ 04_first_python_app/code.py
@@ -3,3 +3,12 @@ age_number = int(user_age)
 
 months = age_number * 12
 print(f"{age_number} is equal to {months} months.")
+
+
+# The other form, @roxana
+user_age = int(input("Enter your age: "))
+months = user_age * 12
+days = months * 30
+hours = days * 24
+minute = hours * 60
+print(f"{user_age} is equal to {months} months and {days} days and {hours} hours and {minute} minutes")
