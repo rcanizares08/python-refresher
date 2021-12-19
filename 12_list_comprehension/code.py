544fb7bc4997f92ba9b149264618281f8b4070e4
roxana <rcanizares08@gmail.com>
2021-12-19 10:55:22 +0100

    Add action List comprehension

--- 12_list_comprehension/code.py
+++ 12_list_comprehension/code.py
@@ -29,3 +29,28 @@ print(friends)
 print(starts_s)
 print(friends is starts_s)
 print("friends: ", id(friends), " starts_s: ", id(starts_s))
+
+
+# -- Can make a new list of friends whose name not starts with S -- @Roxana
+
+friends = ["Rolf", "Sam", "Samantha", "Saurabh", "Jen"]
+starts_s = [friend for friend in friends if friend.startswith("S") == False]
+
+print(starts_s)
+
+
+# Other method
+song = "cold, cold heart"
+
+# replacing 'cold' with 'hurt'
+print(song.replace("cold", "hurt"))
+
+song = "Let it be, let it be, let it be, let it be"
+
+# replacing only of 'let'
+print(song.replace("let", "don't let", 2))
+
+ls = ["Let it be", "let it be", "let it be", "let it be"]
+l_new = [l.replace("let", "don't let") for l in ls]
+print(l_new)
+print(",".join(l_new))
