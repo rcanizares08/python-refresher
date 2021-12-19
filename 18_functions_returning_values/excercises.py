b8eb9f0c29dff8b5b68baaaf96be6acb4c097d3b
roxana <rcanizares08@gmail.com>
2021-12-19 12:16:54 +0100

    functions return excercises

---
+++ 18_functions_returning_values/excercises.py
@@ -0,0 +1,28 @@
+# Complete the function by making sure it returns 42. .
+def return_42():
+    # Complete function here
+    return 42
+
+
+def return_42_1(y=42):
+    # Complete function here
+    return print(y)
+
+
+# Create a function below, called my_function, that takes two arguments and returns the result of its two arguments multiplied together.
+
+
+def my_function(x, y):
+    return x * y
+
+
+print(return_42())
+print(my_function(5, 4))
+
+
+def my_function1(x=20, y=10):
+    return print(x * y)
+
+
+my_function1()
+return_42_1()
